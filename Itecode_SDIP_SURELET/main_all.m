6183e232f9e64af51ace8395035b8f6c23367033
Shogo MURAMATSU <shogo@eng.niigata-u.ac.jp>
2025-04-20 13:44:45 +0900

    Revised to read a Kodim set.

---
+++ Itecode_SDIP_SURELET/main_all.m
@@ -0,0 +1,8 @@
+setup
+
+main_channel_mean %.mlx
+main_nsolt_sdip %.mlx
+main_nsolt_sdip_sure_Interscale_gray %.mlx
+main_nsolt_sdip_sure_Interscale_color %.mlx
+main_nsolt_lista %.mlx
+main_nsolt_sdip_sure_ITEjoural_interchannel %.mlx 
